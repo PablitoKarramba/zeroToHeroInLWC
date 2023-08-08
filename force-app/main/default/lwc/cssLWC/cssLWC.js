28d371ccc61e8aebfffb6e16b6140331e6353855
Paweł Galiński <pawel.galinski@vrpconsulting.com>
2023-08-08 16:56:46 +0200

    Added dynamic and shadom DOM styling components

--- force-app/main/default/lwc/cssLWC/cssLWC.js
+++ force-app/main/default/lwc/cssLWC/cssLWC.js
@@ -1,3 +1,21 @@
 import { LightningElement } from "lwc";
 
-export default class CssLWC extends LightningElement {}
+export default class CssLWC extends LightningElement {
+    percent = 0;
+    changeHandler(event) {
+        this.percent = event.target.value;
+    }
+
+    get percentage() {
+        return `width: ${this.percent}%;`;
+    }
+
+    renderedCallback() {
+        const style = document.createElement("style");
+        style.innerText = `c-shadom-dom-styling .slds-button{
+            background: red;
+            color: white;
+        }`;
+        this.template.querySelector("lightning-button").appendChild(style);
+    }
+}
